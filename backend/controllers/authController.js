2f3e81ff165a4a7b707c4f46bcc0d7fcec03a6c0
pawandeepf01-eng <pawandeep.f01@gmail.com>
2026-06-10 13:30:49 +0530

    first

---
+++ backend/controllers/authController.js
@@ -0,0 +1,121 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+
+const register = async (req, res) => {
+  try {
+    const {
+      name,
+      phone,
+      password,
+      role,
+      serviceType,
+    } = req.body;
+
+    const exists = await User.findOne({ phone });
+
+    if (exists) {
+      return res.status(400).json({
+        success: false,
+        message: "User Already Exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(
+      password,
+      10
+    );
+
+    const user = await User.create({
+      name,
+      phone,
+      password: hashedPassword,
+      role,
+      serviceType,
+    });
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "Registration Successful",
+      token,
+      user,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// Login
+
+const login = async (req, res) => {
+  try {
+    const { phone, password } = req.body;
+
+    const user = await User.findOne({
+      phone,
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User Not Found",
+      });
+    }
+
+    const match =
+      await bcrypt.compare(
+        password,
+        user.password
+      );
+
+    if (!match) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid Password",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.json({
+      success: true,
+      token,
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  register,
+  login,
+};
