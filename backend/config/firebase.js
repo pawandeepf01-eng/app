04553fdae29216553043128d97e31f93d0cab3bd
pawandeepf01-eng <pawandeep.f01@gmail.com>
2026-07-08 16:47:54 +0530

    Use Firebase env variables

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,9 +1,11 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 
-const serviceAccount = require("../servicehub-7734d-firebase-adminsdk-fbsvc-954e2b3437.json");
-
 const app = initializeApp({
-  credential: cert(serviceAccount),
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
 });
 
 module.exports = app;
